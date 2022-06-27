979748d82a6a56c309f3bee7eb1f22ad27ef6921
Shashikarhale123 <95854325+Shashikarhale123@users.noreply.github.com>
2022-06-27 15:57:53 +0530

    Add files via upload

---
+++ com.inn.banking/src/test/java/com/inn/banking/OnlineBankingManagementApplicationTests.java
@@ -0,0 +1,43 @@
+package com.inn.banking;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+
+import com.inn.banking.dao.AppointmentDao;
+import com.inn.banking.rest.AppUserRest;
+import com.inn.banking.rest.AppointmentRest;
+
+
+@SpringBootTest
+class OnlineBankingManagementApplicationTests {
+
+	@Autowired
+	AppUserRest appUserRest;
+	 AppointmentRest  appointmentRest;
+	 AppointmentDao  appointmentDao;
+	@Test
+	void contexLoads() {
+		
+	}
+	@Test
+    void login() {
+        Map<String, String> requestMap = new HashMap<>();
+        requestMap.put("username", "ashish");
+        requestMap.put("password", "1234");
+        HttpStatus statusCode = appUserRest.login(requestMap).getStatusCode();
+        Assertions.assertTrue(statusCode == HttpStatus.OK);
+        
+    }
+	
+	}
+
